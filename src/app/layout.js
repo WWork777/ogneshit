59c32cb4f568d8da0aaf8dbc9621c15f0beb3e7d
id0ntc4re <parsikovevgenij470@gmail.com>
2025-12-21 11:19:06 +0700

    yandex metrika

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,55 +1,57 @@
-import { Montserrat, Manrope } from 'next/font/google';
-import './globals.css';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import { Montserrat, Manrope } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import YandexMetrika from "@/components/YandexMetrika/YandexMEtrika";
 
 const montserrat = Montserrat({
-  variable: '--font-montserrat',
-  subsets: ['latin', 'cyrillic'],
-  display: 'swap',
-  weight: ['300', '400', '500', '600', '700'],
+  variable: "--font-montserrat",
+  subsets: ["latin", "cyrillic"],
+  display: "swap",
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 const manrope = Manrope({
-  variable: '--font-manrope',
-  subsets: ['latin', 'cyrillic'],
-  display: 'swap',
+  variable: "--font-manrope",
+  subsets: ["latin", "cyrillic"],
+  display: "swap",
 });
 
 export const metadata = {
   icons: {
     icon: [
       {
-        rel: 'icon',
-        type: 'image/x-icon',
-        url: '/favicon.ico',
+        rel: "icon",
+        type: "image/x-icon",
+        url: "/favicon.ico",
       },
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        url: '/favicon/favicon-32x32.png',
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon/favicon-32x32.png",
       },
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        url: '/favicon/favicon-16x16.png',
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon/favicon-16x16.png",
       },
     ],
-    shortcut: '/favicon.ico',
-    apple: '/favicon/apple-touch-icon.png',
+    shortcut: "/favicon.ico",
+    apple: "/favicon/apple-touch-icon.png",
   },
-  manifest: '/favicon/site.webmanifest',
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='ru'>
+    <html lang="ru">
       <body className={`${montserrat.variable} ${manrope.variable}`}>
         <Header />
         {children}
         <Footer />
+        <YandexMetrika />
       </body>
     </html>
   );
